9f234acf34c4d393a1b03337cb74ddeca89d7115
YuroRyuzaki <football11euro@icloud.com>
2021-09-17 17:35:38 +0900

    SIC用のrepository作製

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SIC' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SIC
+  pod 'Firebase/Analytics'
+
+  target 'SICTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SICUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ SIC/NewCustomerViewController.swift
@@ -0,0 +1,66 @@
+//
+//  NewCustomerViewController.swift
+//  SIC
+//
+//  Created by Euro Ryuzaki on 2021/09/13.
+//
+
+import UIKit
+
+class NewCustomerViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var birthday: UITextField!
+   
+    
+    let datePicker = UIDatePicker()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       createDatePicker()
+        // Do any additional setup after loading the view.
+    }
+    
+    //生年月日設定スタート
+    func createDatePicker(){
+        
+        birthday.textAlignment = .center
+    //toolber
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //bar button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target:nil , action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        //assign toolbar
+        birthday.inputAccessoryView = toolbar
+        
+        //assign date picker to the text filed
+        birthday.inputView = datePicker
+        
+        //date picker mode
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ja-JP")
+    }
+    
+    @objc func donePressed() {
+        //firmatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        
+        birthday.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    //生年月日設定スタート
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
